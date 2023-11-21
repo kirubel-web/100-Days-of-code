cd5f3deeced7f36f8e99e2812a236e71a647dec4
kirubel-web <kirubelalemu119@gmail.com>
2023-11-21 21:19:12 +0300

    project of day one completed

---
+++ Day-1/project.py
@@ -0,0 +1,6 @@
+# Day one final project
+
+print('Welcome to the Band Name Generator.')
+city = input("What's name of the city you grew up in? ")
+pet = input("What's your pet's name? ")
+print('Your band name could be ' + city + " " + pet)
