bffac0bb995ef21b62e81027e32328ba37207292
kirubel-web <kirubelalemu119@gmail.com>
2023-11-22 21:15:34 +0300

    BMI calculator

---
+++ Day-2/BMI_calc.py
@@ -0,0 +1,10 @@
+# program that calculates the body mass index
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+height = float(height)
+weight = float(weight)
+bmi = weight / (height ** 2)
+
+print(int(bmi))
