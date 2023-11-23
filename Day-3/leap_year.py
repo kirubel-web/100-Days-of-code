f386ab280920d89d1df8d0530dc76cf1f300cadb
kirubel-web <kirubelalemu119@gmail.com>
2023-11-23 23:07:10 +0300

    conditionals with leap year

--- Day-3/leap_year.py
+++ Day-3/leap_year.py
@@ -1,1 +1,20 @@
 # program that works out whether if a given year is a leap year
+
+year = int(input("Which year do you want to check? "))
+# solution 1
+""" if year % 4 == 0 and year % 400 == 0 and year % 100 == 0:
+    print('Leap')
+else:
+    print('Not') """
+
+# solution 2
+if year % 4 == 0:
+    if year % 100 != 0:
+        if year % 400 == 0:
+            print('leap')
+        else:
+            print('Not Leap')
+    else:
+        print('Not Leap')
+else:
+    print('Not Leap')
