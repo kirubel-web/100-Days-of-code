2e9bddaf05dd6dbdc63863a243861db923844a85
kirubel-web <kirubelalemu119@gmail.com>
2023-11-23 21:16:27 +0300

    day three

---
+++ Day-3/odd_even.py
@@ -0,0 +1,8 @@
+# program that works out whether if a given number is an odd or even
+
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+    print('even')
+else:
+    print('odd')
