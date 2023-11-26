6d1632af7e4ca58b92c22c3484139aba3aa4df27
kirubel-web <kirubelalemu119@gmail.com>
2023-11-26 21:07:51 +0300

    love calculator done

--- Day-3/love_calculator.py
+++ Day-3/love_calculator.py
@@ -7,8 +7,27 @@ name2 = input("What is their name? \n")
 name1 = name1.lower()
 name2 = name2.lower()
 
-love = ['t', 'r', 'u', 'e', 'l', 'o', 'v', 'e']
-i = 0
-if i in love and name1:
-    i += 1
-print(i)
+full_name = name1 + name2
+
+t = full_name.count('t')
+r = full_name.count('r')
+u = full_name.count('u')
+e = full_name.count('e')
+
+total_1 = t + r + u + e
+
+l = full_name.count('l') # noqa
+o = full_name.count('o')
+v = full_name.count('v')
+e = full_name.count('e')
+
+total_2 = l + o + v + e
+
+total = str(total_1) + str(total_2)
+
+if (int(total) < 10) or (int(total) > 90):
+    print(f'Your score is {total}, you go together like coke and mentos.')
+elif 40 < int(total) < 50:
+    print(f'Your score is {total}, you are alright together.')
+else:
+    print(f'Your score is {total}')
