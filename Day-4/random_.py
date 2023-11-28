5c0795614926cf29ea202409238d37ff668b0b2b
kirubel-web <kirubelalemu119@gmail.com>
2023-11-28 22:35:48 +0300

    done

--- Day-4/random_.py
+++ Day-4/random_.py
@@ -3,4 +3,10 @@ import my_module
 
 random_integer = random.randint(1, 10)
 print(random_integer)
-print(my_module.pi)
+print(my_module)
+
+random_float = random.random()
+print(random_float * 5)
+
+love_score = random.randint(1, 100)
+print(f'Your love score is {love_score}')
