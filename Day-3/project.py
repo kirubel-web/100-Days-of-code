20a9ce14441c9ec0dce510c656b4dea2b9bed38b
kirubel-web <kirubelalemu119@gmail.com>
2023-11-28 22:21:42 +0300

    finished project

--- Day-3/project.py
+++ Day-3/project.py
@@ -6,7 +6,8 @@ print("Your mission is to find the treasure.")
 decision_1 = input('You are at a cross road. left or right.\n').lower()
 
 if decision_1 == 'left':
-    print('You come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.')
+    print('You come to a lake. There is an island in the middle of the lake. \
+Type "wait" to wait for a boat. Type "swim" to swim across.')
     decision_2 = input('\n').lower()
     if decision_2 == 'wait':
         print('You arrive at the island unharmed. There is a house with 3 \
@@ -21,4 +22,4 @@ doors. One red, one yellow and one blue. which color do you choose')
 elif decision_1 == 'right':
     print('Game Over.')
 else:
-    pass
+    print('Game Over.')
