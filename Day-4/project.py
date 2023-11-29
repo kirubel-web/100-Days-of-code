61bf5bbbe05efc75988ba8c3df70a6276ce50dc5
kirubel-web <kirubelalemu119@gmail.com>
2023-11-29 19:15:27 +0300

    invalid input control

--- Day-4/project.py
+++ Day-4/project.py
@@ -2,11 +2,13 @@
 import random
 
 player_choice = input('What do you choose? Type 0 for Rock, 1 for Paper or 2  \
-    for Scissors.\n')
+for Scissors.\n')
 computer = random.randint(0, 2)
 try:
     if int(player_choice) == computer:
         print('Its a Draw')
+    elif int(player_choice) >= 3:
+        print('Invalid Input Please Try again.')
     elif int(player_choice) == 0 and computer == 1:
         print('You Lose computer Choose paper')
     elif int(player_choice) == 0 and computer == 2:
